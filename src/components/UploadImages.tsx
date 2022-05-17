415e29b8c372b6f2cddaf13cc87fd61eaf7fd04f
tomoyuki-hiranuma <numanumatom@gmail.com>
2022-05-17 19:21:58 +0900

    first commit

---
+++ src/components/UploadImages.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react"
+
+
+export const UploadImages  = () : JSX.Element => {
+  return (
+    <Box>
+      Input
+    </Box>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { UploadImages } from '../components/UploadImages'
+
+const Home: NextPage = () => {
+  return (
+    <>
+     <UploadImages />
+    </>
+  )
+}
+
+export default Home
