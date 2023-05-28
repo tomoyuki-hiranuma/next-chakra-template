7997c42c39da50e59d19feaa89e241d1302f56c2
tomoyuki-hiranuma <numanumatom@gmail.com>
2023-05-28 21:19:19 +0900

    run prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next';
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <>Home</>;
-};
+  return <>Home</>
+}
 
-export default Home;
+export default Home
